c246e5f2fe7f2048f68ef803084689586c7dc9bf
NOLTEPRO <114636654+NOLTEPRO@users.noreply.github.com>
2023-07-19 10:43:40 +0000

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -7,11 +7,16 @@ load_dotenv()
 que = {}
 admins = {}
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
+API_ID = int(getenv("API_ID", "22661813"))
+API_HASH = getenv("API_HASH", "b8bdd0f7ca34c25ae62e8e03458c985c")
+BOT_TOKEN = getenv("BOT_TOKEN", "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")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "300"))
-STRING_SESSION = getenv("STRING_SESSION")
+STRING_SESSION = getenv("STRING_SESSION", "MIIBCgKCAQEAyMEdY1aR+sCR3ZSJrtztKTKqigvO/vBfqACJLZtS7QMgCGXJ6XIR
+yy7mx66W0/sOFa7/1mAZtEoIokDP3ShoqF4fVNb6XeqgQfaUHd8wJpDWHcR2OFwv
+plUUI1PLTktZ9uW2WE23b+ixNwJjJGwBDJPQEQFBE+vfmH0JP503wr5INS1poWg/
+j25sIWeYPHYeOrFp/eXaqhISP6G+q2IeTaWTXpwZj4LzXq5YOpk4bYEQ6mvRq7D1
+aHWfYmlEGepfaYR8Q0YqvvhYtMte3ITnuSJs171+GDqpdKcSwHnd6FudwGO4pcCO
+j4WcDuXc2CTHgH8gFTNhp/Y8/SpDOhvn9QIDAQAB")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1282754256").split()))
 aiohttpsession = aiohttp.ClientSession()
